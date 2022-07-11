84f6766b02026cfe9f25a3ebd64bc7f684d82999
Ronald HA <ronaldhuamanatalaya@gmail.com>
2022-07-10 23:45:31 -0500

    Tiktok-clone

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <main>
+        Tiktok Clone
+      </main>
+
+    </div>
+  )
+}
+
+export default App
